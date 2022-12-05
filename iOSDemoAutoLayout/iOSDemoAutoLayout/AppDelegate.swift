c9250b4f12a1e653e18848ba14326e1ee9a419e9
Viet Huy <tranviethuy01@gmail.com>
2022-12-05 21:56:52 +0700

    add demo project

---
+++ iOSDemoAutoLayout/iOSDemoAutoLayout/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  iOSDemoAutoLayout
+//
+//  Created by Viet Huy on 12/5/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+}
+
---
+++ iOSDemoAutoLayout/iOSDemoAutoLayout/MainScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainScreenViewController.swift
+//  iOSDemoAutoLayout
+//
+//  Created by Viet Huy on 12/5/22.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
